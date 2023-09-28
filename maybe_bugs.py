5e9a30a44e8d1e0b8ebdd977ed2fb027766e2beb
Matt Guo <matt@mattguo.com>
2023-09-28 14:02:34 -0700

    Update maybe_bugs.py

--- maybe_bugs.py
+++ maybe_bugs.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-
+# The issue is, no matter if I clicked which of the 3 buttons, I always got "clicked 2" output 
 for i in range(3):
     st.button(f"Button {i}", key=f"btn_{i}", on_click=lambda: st.warning(f"clicked {i}"))
